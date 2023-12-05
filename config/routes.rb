1556b7b37defced7f7cebde74aad83b33676b91f
ippei-shimizu <ippei.shimizu.32@gmail.com>
2023-12-06 00:51:44 +0900

    ✨ Add: letter_opener_web設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
   mount_devise_token_auth_for 'User', at: 'auth'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  
+  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
 end
