71d1ca857cfb81cd944d0b7d49fd1537789141b9
ippei-shimizu <ippei.shimizu.32@gmail.com>
2023-12-07 00:51:10 +0900

    ✨ Add: ユーザー認証機能API作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,15 @@
 Rails.application.routes.draw do
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
-  mount_devise_token_auth_for 'User', at: 'auth'
+
+  namespace :api do
+    namespace :v1 do
+      mount_devise_token_auth_for 'User', at: 'auth', controllers: {
+        registrations: 'api/v1/auth/registrations'
+      }
+
+      namespace :auth do
+        resources :sessions, only: [:index]
+      end
+    end
+  end
 end
