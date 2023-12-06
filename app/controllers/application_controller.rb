7a985a02cde4f0d37f9dd7f7ed0c075d6fdda3c2
ippei-shimizu <ippei.shimizu.32@gmail.com>
2023-12-07 01:52:41 +0900

    🎨 rubocop

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,11 @@
 class ApplicationController < ActionController::API
-        include DeviseTokenAuth::Concerns::SetUserByToken
-        before_action do
-                I18n.locale = :ja
-        end
-        before_action :configure_permitted_parameters, if: :devise_controller?
+  include DeviseTokenAuth::Concerns::SetUserByToken
+  before_action do
+    I18n.locale = :ja
+  end
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
-        def configure_permitted_parameters
-                devise_parameter_sanitizer.permit(:account_update, keys: [:name, :user_id])
-        end
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:account_update, keys: %i[name user_id])
+  end
 end
