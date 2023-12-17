0ce85377803d55fa203679c7f59219e423a7c826
Ippei Shimizu <ippei.shimizu.32@gmail.com>
2023-12-18 00:53:07 +0900

    ✨ Add: ユーザー登録完了メール送信設定

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,9 +38,9 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  config.action_mailer.default_options = { from: 'no-replay@example.com' }
+  config.action_mailer.default_url_options = { host: 'localhost:3000' }
   config.action_mailer.delivery_method = :letter_opener_web
-
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3001 }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
