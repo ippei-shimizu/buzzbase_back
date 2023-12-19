f4a50a25bd2ff71b3de12e8575012b8acbf5f97c
Ippei Shimizu <ippei.shimizu.32@gmail.com>
2023-12-19 09:07:32 +0900

    🐛 Fix: 15 DisabledSessionError回避

---
+++ app/controllers/concerns/devise_hack_fake_session.rb
@@ -0,0 +1,23 @@
+module DeviseHackFakeSession
+  extend ActiveSupport::Concern
+
+  class FakeSession < Hash
+    def enabled?
+      false
+    end
+
+    def destroy; end
+  end
+
+  included do
+    before_action :set_fake_session
+
+    private
+
+    def set_fake_session
+      return unless Rails.configuration.respond_to?(:api_only) && Rails.configuration.api_only
+
+      request.env['rack.session'] ||= ::DeviseHackFakeSession::FakeSession.new
+    end
+  end
+end
