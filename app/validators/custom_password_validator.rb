1c21ae4033c3ef457c5226fa9f717637008209fb
Ippei Shimizu <ippei.shimizu.32@gmail.com>
2023-12-21 21:34:11 +0900

    🎨 15 rubocop

--- app/validators/custom_password_validator.rb
+++ app/validators/custom_password_validator.rb
@@ -2,9 +2,8 @@ class CustomPasswordValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
     return if value.blank?
 
-    unless value =~ /\A[a-zA-Z0-9]+\z/
-      record.errors.add(attribute, :invalid, message: 'は半角英数字のみ使用できます')
-    end
-    
+    return if value =~ /\A[a-zA-Z0-9]+\z/
+
+    record.errors.add(attribute, :invalid, message: 'は半角英数字のみ使用できます')
   end
 end
