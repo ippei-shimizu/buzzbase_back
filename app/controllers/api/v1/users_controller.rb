0681e8b0f6c349d870b8c1a669eb7d1f6e1379bf
Ippei Shimizu <ippei.shimizu.32@gmail.com>
2023-12-23 01:58:50 +0900

    🎨 Fix: 14 rubocop

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -2,12 +2,12 @@ module Api
   module V1
     class UsersController < ApplicationController
       before_action :authenticate_api_v1_user!
-      
+
       def update
         if current_api_v1_user.update(user_params)
-          render json: {success: true}
+          render json: { success: true }
         else
-          render json: {erros: current_api_v1_user.errors.full_messages},status: :unprocessable_entity
+          render json: { erros: current_api_v1_user.errors.full_messages }, status: :unprocessable_entity
         end
       end
 
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,5 +10,4 @@ class ApplicationController < ActionController::API
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:account_update, keys: %i[name user_id])
   end
-
 end
