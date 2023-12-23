17a3c8a8c185d180510ab321aaa8192766aa459a
Ippei Shimizu <ippei.shimizu.32@gmail.com>
2023-12-23 21:47:42 +0900

    🐛 Fix: 14 user_idをユニーク

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_12_11_080121) do
+ActiveRecord::Schema[7.0].define(version: 2023_12_23_124318) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -30,14 +30,15 @@ ActiveRecord::Schema[7.0].define(version: 2023_12_11_080121) do
     t.string "nickname"
     t.string "image"
     t.string "email"
-    t.string "user_id"
     t.json "tokens"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "user_id"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["uid", "provider"], name: "index_users_on_uid_and_provider", unique: true
+    t.index ["user_id"], name: "index_users_on_user_id", unique: true
   end
 
 end
