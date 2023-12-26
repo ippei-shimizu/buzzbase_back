1aff65c9142466a6ebf3cbfe98a529a9d426b0e0
Ippei Shimizu <ippei.shimizu.32@gmail.com>
2023-12-26 20:28:22 +0900

    🎨 Fix: 19 rubocop

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -14,11 +14,11 @@ class AvatarUploader < CarrierWave::Uploader::Base
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
-  def default_url(*args)
+  def default_url(*_args)
     # For Rails 3.1+ asset pipeline compatibility:
     # ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
-  
-    "/images/profile/user-default-yellow.svg"
+
+    '/images/profile/user-default-yellow.svg'
   end
 
   # Process files as they are uploaded:
