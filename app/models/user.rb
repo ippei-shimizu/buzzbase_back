b513df922a3f6ee362053373dd6ed9b1dd0bf90f
Ippei Shimizu <ippei.shimizu.32@gmail.com>
2023-12-26 09:30:17 +0900

    🐛 Fix: 19 デフォルト画像をランダムに設定するのをやめる

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,17 +8,17 @@ class User < ActiveRecord::Base
   validates :password, custom_password: true, on: :create
   validates :user_id, uniqueness: true, allow_blank: true
 
-  before_create :set_default_user_icon
+  # before_create :set_default_user_icon
 
-  private
+  # private
 
-  def set_default_user_icon
-    return if image.present?
+  # def set_default_user_icon
+  #   return if image.present?
 
-    images = ['user-default-yellow.svg', 'user-default-cyan.svg', 'user-default-pink.svg', 'user-default-green.svg', 'user-default-purple.svg',
-              'user-default-blue.svg']
-    default_image_path = Rails.public_path.join('images', 'profile', images.sample)
-    Rails.logger.debug default_image_path.to_s
-    self.image = File.open(default_image_path)
-  end
+  #   images = ['user-default-yellow.svg', 'user-default-cyan.svg', 'user-default-pink.svg', 'user-default-green.svg', 'user-default-purple.svg',
+  #             'user-default-blue.svg']
+  #   default_image_path = Rails.public_path.join('images', 'profile', images.sample)
+  #   Rails.logger.debug default_image_path.to_s
+  #   self.image = File.open(default_image_path)
+  # end
 end
