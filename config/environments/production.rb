f2f5544c75248388cb9e3a56219a6fdc2f681c67
Ippei Shimizu <ippei.shimizu.32@gmail.com>
2024-01-10 07:59:56 +0900

    🩹 Fix: rubocop

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -57,7 +57,7 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
-  Rails.application.config.hosts << "buzzbase-front.vercel.app"
+  Rails.application.config.hosts << 'buzzbase-front.vercel.app'
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
