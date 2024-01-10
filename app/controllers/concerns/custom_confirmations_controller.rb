6a06e12af4e33e1137fc3dfbd7a8d0b0964168a6
Ippei Shimizu <ippei.shimizu.32@gmail.com>
2024-01-10 22:59:31 +0900

    🐛 Fix: リダイレクト処理

--- app/controllers/concerns/custom_confirmations_controller.rb
+++ app/controllers/concerns/custom_confirmations_controller.rb
@@ -1,14 +1,18 @@
 class CustomConfirmationsController < DeviseTokenAuth::ConfirmationsController
   def show
     super do |resource|
-      @redirect_url = your_custom_path(resource)
-      render json: { redirect_url: @redirect_url }, status: :ok
+      if resource.errors.empty?
+        redirect_url = your_custom_path(resource)
+        render json: { message: 'Confirmation successful.', redirect_url: redirect_url }, status: :ok
+      else
+        render json: { message: resource.errors.full_messages.join(', ') }, status: :unprocessable_entity
+      end
     end
   end
 
   private
 
-  def your_custom_path(_resource)
+  def your_custom_path(resource)
     token = params[:confirmation_token]
     "#{ENV.fetch('CONFIRM_SUCCESS_URL', nil)}?confirmation_token=#{token}"
   end
