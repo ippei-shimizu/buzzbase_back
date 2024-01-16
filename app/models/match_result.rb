69f393757276b7350cbeb51715f6fa3fffd8bbfe
Ippei Shimizu <ippei.shimizu.32@gmail.com>
2024-01-17 07:44:16 +0900

    ✨ Add: 23 リレーション設定

--- app/models/match_result.rb
+++ app/models/match_result.rb
@@ -1,2 +1,6 @@
 class MatchResult < ApplicationRecord
+  belongs_to :user
+  belongs_to :my_team, class_name: 'Team'
+  belongs_to :opponent_team, class_name: 'Team'
+  belongs_to :tournament, optional: true
 end
