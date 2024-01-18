90418715947705ef8c44962c97f152a798dd5de5
Ippei Shimizu <ippei.shimizu.32@gmail.com>
2024-01-19 01:52:41 +0900

    🐛 Fix: 23 バリデーション削除

--- app/models/team.rb
+++ app/models/team.rb
@@ -4,6 +4,4 @@ class Team < ApplicationRecord
   has_one :user, foreign_key: 'user_id', primary_key: 'id', dependent: :destroy, inverse_of: :team
 
   validates :name, presence: true
-  validates :category_id, presence: true
-  validates :prefecture_id, presence: true
 end
