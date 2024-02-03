c3ee89207e1669752326e3b21ce8a6b5bb5949a4
Ippei Shimizu <ippei.shimizu.32@gmail.com>
2024-02-03 15:38:39 +0900

    ✨ Add: 27 グループアイコンアップロード設定

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,4 +1,5 @@
 class Group < ApplicationRecord
+  mount_uploader :icon, GroupIconUploader
   has_many :group_users, dependent: :destroy
   has_many :users, through: :group_users
   has_many :group_invitations, dependent: :destroy
