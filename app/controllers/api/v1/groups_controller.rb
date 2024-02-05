7b1c2f3aa254d012c99f9d3e77657718d8c0cd42
Ippei Shimizu <ippei.shimizu.32@gmail.com>
2024-02-06 00:54:12 +0900

    🐛 Fix: フォローユーザー招待

--- app/controllers/api/v1/groups_controller.rb
+++ app/controllers/api/v1/groups_controller.rb
@@ -7,7 +7,7 @@ module Api
         group = current_api_v1_user.groups.build(group_params)
         if group.save
           invite_users(group, invite_user_ids_params)
-          render json: group, status: :create
+          render json: group, status: :created
         else
           render json: { erros: group.errors.full_messages }, status: :unprocessable_entity
         end
@@ -22,6 +22,13 @@ module Api
       def invite_user_ids_params
         params[:invite_user_ids] || []
       end
+      def invite_users(group, user_ids)
+        user_ids.each do |user_id|
+          user = User.find_by(id: user_id)
+          group.group_invitations.create(user:, state: 'pending', sent_at: Time.current) if user && current_api_v1_user.following.include?(user)
+        end
+      end
+
     end
   end
 end
--- app/models/group.rb
+++ app/models/group.rb
@@ -3,11 +3,4 @@ class Group < ApplicationRecord
   has_many :group_users, dependent: :destroy
   has_many :users, through: :group_users
   has_many :group_invitations, dependent: :destroy
-
-  def invite_users(group, user_ids)
-    user_ids.each do |user_id|
-      user = User.find_by(id: user_id)
-      group.group_invitations.create(user:, state: 'pending', sent_at: Time.current) if user && current_api_v1_user.following.include?(user)
-    end
-  end
 end
