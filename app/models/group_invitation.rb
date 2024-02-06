5097aa660fb19f1b53ec5598375b895d3fedfcf9
Ippei Shimizu <ippei.shimizu.32@gmail.com>
2024-02-06 23:15:51 +0900

    🐛 Fix: rubocop

--- app/models/group_invitation.rb
+++ app/models/group_invitation.rb
@@ -2,8 +2,6 @@ class GroupInvitation < ApplicationRecord
   belongs_to :user
   belongs_to :group
 
-  validates :user_id, presence: true
-  validates :group_id, presence: true
   validates :state, presence: true
 
   enum state: { pending: 0, accepted: 1, declined: 2 }
