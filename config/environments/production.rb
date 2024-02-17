8b41375ee50654e909df3f71c02fc7f1ec87cc8a
Ippei Shimizu <ippei.shimizu.32@gmail.com>
2024-02-17 16:01:39 +0900

    ✨ Add: ドメイン設定

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -57,12 +57,12 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
-  Rails.application.config.hosts << 'buzzbase-front.vercel.app'
+  Rails.application.config.hosts << 'buzzbase.jp'
   config.hosts << 'buzzbaseback.fly.dev'
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  config.action_mailer.default_url_options = { host: 'buzzbase-front.vercel.app', protocol: 'https' }
+  config.action_mailer.default_url_options = { host: 'buzzbase.jp', protocol: 'https' }
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.perform_deliveries = true
   config.action_mailer.delivery_method = :smtp
