5662ef92d9d3af1617d1e4bff242f3d5a12d9396
Ippei Shimizu <ippei.shimizu.32@gmail.com>
2024-02-20 01:25:45 +0900

    🎨 Fix: rubocop

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -7,15 +7,13 @@ CarrierWave.configure do |config|
     config.fog_provider = 'fog/aws'
     config.fog_credentials = {
       provider: 'AWS',
-      aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
-      aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+      aws_access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID', nil),
+      aws_secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY', nil),
       region: 'ap-northeast-1'
     }
     config.fog_directory = "ENV['AWS_BUCKET_NAME']"
-    config.cache_storage = :fog 
+    config.cache_storage = :fog
   else
-    config.storage = :file 
+    config.storage = :file
   end
-
 end
-
