b5ce880f0ce559dab6ea134ea7a8b514845f50ef
Ippei Shimizu <ippei.shimizu.32@gmail.com>
2026-04-04 22:57:10 +0900

    Fix: マイグレーションのexecuteを個別SQL文に分割

--- db/migrate/20260404100002_normalize_match_type_values.rb
+++ db/migrate/20260404100002_normalize_match_type_values.rb
@@ -1,9 +1,7 @@
 class NormalizeMatchTypeValues < ActiveRecord::Migration[7.0]
   def up
-    execute <<-SQL.squish
-      UPDATE match_results SET match_type = 'regular' WHERE match_type = '公式戦';
-      UPDATE match_results SET match_type = 'open' WHERE match_type = 'オープン戦';
-    SQL
+    execute "UPDATE match_results SET match_type = 'regular' WHERE match_type = '公式戦'"
+    execute "UPDATE match_results SET match_type = 'open' WHERE match_type = 'オープン戦'"
   end
 
   def down
