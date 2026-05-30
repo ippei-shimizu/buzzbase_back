3d7cf75bd7a0e361e90fc61d8035796faa396897
Ippei Shimizu <ippei.shimizu.32@gmail.com>
2026-05-30 23:40:03 +0900

    Add: stadiums.name に prefecture_id スコープの一意性バリデーションを追加
    同一都道府県内での同名球場登録を防ぐ。prefecture_id が NULL の場合は重複を許容する（県不明データ向け）。

--- spec/models/stadium_spec.rb
+++ spec/models/stadium_spec.rb
@@ -12,5 +12,29 @@ RSpec.describe Stadium, type: :model do
 
     it { is_expected.to validate_presence_of(:name) }
     it { is_expected.to validate_length_of(:name).is_at_most(100) }
+
+    describe '同一都道府県内での name 一意性' do
+      let(:prefecture) { Prefecture.create!(name: 'テスト県') }
+
+      it '同じ prefecture_id で同名は登録できない' do
+        described_class.create!(name: '東京ドーム', prefecture:)
+        duplicate = described_class.new(name: '東京ドーム', prefecture:)
+        expect(duplicate).not_to be_valid
+        expect(duplicate.errors[:name]).to be_present
+      end
+
+      it '異なる prefecture_id では同名でも登録できる' do
+        other_prefecture = Prefecture.create!(name: 'もう一つのテスト県')
+        described_class.create!(name: '東京ドーム', prefecture:)
+        another = described_class.new(name: '東京ドーム', prefecture: other_prefecture)
+        expect(another).to be_valid
+      end
+
+      it 'prefecture_id が NULL の場合は重複を許容する' do
+        described_class.create!(name: '謎の球場', prefecture: nil)
+        another = described_class.new(name: '謎の球場', prefecture: nil)
+        expect(another).to be_valid
+      end
+    end
   end
 end
