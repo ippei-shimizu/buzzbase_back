2530033de272a81c5cec93d0814f61ccf4cc38dd
Ippei Shimizu <ippei.shimizu.32@gmail.com>
2026-08-06 22:48:19 +0900

    Fix: GameSummaryService#recent_form の試合日付をJSTで表示するよう修正
    生SQL selectで取得したdate_and_timeがタイムゾーン変換の対象外となり、
    JST早朝(0:00-9:00)の試合が前日として表示されていた。

--- app/services/stats/game_summary_service.rb
+++ app/services/stats/game_summary_service.rb
@@ -83,7 +83,7 @@ module Stats
               .joins('INNER JOIN teams ON teams.id = match_results.opponent_team_id')
               .select(Arel.sql(
                         'game_results.id AS game_result_id, ' \
-                        'match_results.date_and_time, ' \
+                        "#{Stats::JstDateSql::DATE_AND_TIME_JST_SQL} AS date_and_time, " \
                         'match_results.match_type, ' \
                         'teams.name AS opponent_name, ' \
                         'match_results.my_team_score, ' \
